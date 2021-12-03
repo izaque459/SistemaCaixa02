5637974f8207661bbc56da737378ac938c9e45cb
Isaac Santos <isaacdsantos@hotmail.com>
2021-12-03 14:32:37 -0300

    Modificado classe Apresentacao e adicionado metodo informaPagamento a classe Cliente

--- Apresentacao.java
+++ Apresentacao.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 
 import Atendimento.Caixa;
 import Atendimento.Cliente;
+import Atendimento.TipoPagamento;
 
 public class Apresentacao {
     public static void main(String[] args){
@@ -12,22 +13,41 @@ public class Apresentacao {
         Cliente cliente = new Cliente();
         Integer codigo;
         int qte;
+        int pag;
+        boolean resposta;
+
 
 
         while(!caixa.encerraExpediente()){
-            System.out.print("Digite o codigo o produto: ");
-            codigo = entrada.nextInt();
-            System.out.print("Digite a quantidade: ");
-            qte = entrada.nextInt();
-            if(caixa.adicionaProduto(codigo, qte)){
-                
-                cliente.compra(caixa.designaProduto());
-                System.out.println(cliente.exibeCompras());
-                System.out.println("Produto adiconado com sucesso.");
-            }else
-                System.out.println("Produto indisponivel ou quantidade errada ");
+            while(cliente.adicionaItens()){
+                System.out.print("Digite o codigo o produto: ");
+                codigo = entrada.nextInt();
+                System.out.print("Digite a quantidade: ");
+                qte = entrada.nextInt();
+                if(caixa.adicionaProduto(codigo, qte)){
+                    
+                    cliente.compra(caixa.designaProduto());
+                    System.out.println(cliente.exibeCompras());
+                    System.out.println("Produto adiconado com sucesso.");
+                }else
+                    System.out.println("Produto indisponivel ou quantidade errada ");
 
-            entrada.nextLine();
+                entrada.nextLine();
+                System.out.print("Existe mais produtos? ");
+                resposta = entrada.nextBoolean();
+                cliente.proximaCompra(resposta);
+            }
+            while(!cliente.pagaCompra()){
+                entrada.nextLine();
+                System.out.println("Digite forma de pagamento 1 para Dinheiro ou 2 para Cartao: ");
+                pag = entrada.nextInt();
+                if(pag==1)
+                    cliente.informaPagamento(TipoPagamento.Dinheiro);
+                else if(pag==2)
+                    cliente.informaPagamento(TipoPagamento.CartaoCredito);
+                    else 
+                        System.out.println("Digite opcao valida. ");
+            }
             System.out.print("Digite nome de usuario para finalizar expediente: ");
             entradaUsuario = entrada.nextLine();
             caixa.sinalizaEncerramento(entradaUsuario);
--- Atendimento/Cliente.java
+++ Atendimento/Cliente.java
@@ -3,6 +3,8 @@ package Atendimento;
 public class Cliente implements ICliente{
     private String descricao = new String("Descricao Preco Quantidade "); 
     private boolean adiciona = true;
+    private boolean paga = false;
+    private TipoPagamento pagamento;
 
 
     public void compra(String linha){
@@ -19,5 +21,22 @@ public class Cliente implements ICliente{
 
     public void proximaCompra(boolean resposta){
         this.adiciona = resposta;        
+    }
+    public boolean pagaCompra(){
+        return paga;
+    }
+
+    public void informaPagamento(TipoPagamento pagamento){
+        this.pagamento=pagamento;
+        switch(this.pagamento){
+            case CartaoCredito:
+                paga=true;
+                break;
+            case Dinheiro:
+                paga=true;
+                break;
+            default:
+                break;
+        }
     }
 }
